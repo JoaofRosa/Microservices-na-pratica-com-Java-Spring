17a6de271ed405032df695263c37bc1df6aa8940
joao vitor rosa <99752957+JoaofRosa@users.noreply.github.com>
2024-08-31 17:13:52 -0300

    Add files via upload

---
+++ user/src/main/java/com/ms/user/models/UserModel.java
@@ -0,0 +1,42 @@
+package com.ms.user.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.rmi.server.UID;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_USERs")
+public class UserModel {
+    public static final long serialVersionUID = 1l;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID userId;
+    private String name;
+    private String email;
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
